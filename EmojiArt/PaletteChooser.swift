fb9bb21fa422fac01b3036fe06aa9d240d90e0b6
Dr. Andreas Plagens <andreas@plagens.eu>
2021-10-03 14:04:57 +0200

    after lecture 12

--- EmojiArt/PaletteChooser.swift
+++ EmojiArt/PaletteChooser.swift
@@ -37,11 +37,20 @@ struct PaletteChooser: View {
     
     @ViewBuilder
     var contextMenu: some View {
+        AnimatedActionButton(title: "Edit", systemImage: "pencil") {
+//            editing = true
+            paletteToEdit = store.palette(at: chosenPaletteIndex)
+        }
         AnimatedActionButton(title: "New", systemImage: "plus") {
             store.insertPalette(named: "New", emojis: "", at: chosenPaletteIndex)
+//            editing = true
+            paletteToEdit = store.palette(at: chosenPaletteIndex)
         }
         AnimatedActionButton(title: "Delete", systemImage: "minus.circle") {
             chosenPaletteIndex = store.removePalette(at: chosenPaletteIndex)
+        }
+        AnimatedActionButton(title: "Manager", systemImage: "slider.vertical.3") {
+            managing = true
         }
         gotoMenu
     }
@@ -68,7 +77,20 @@ struct PaletteChooser: View {
         }
         .id(palette.id)
         .transition(rollTransition)
+//        .popover(isPresented: $editing) {
+//            PaletteEditor(palette: $store.palettes[chosenPaletteIndex])
+//        }
+        .popover(item: $paletteToEdit) { palette in
+            PaletteEditor(palette: $store.palettes[chosenPaletteIndex])
+        }
+        .sheet(isPresented: $managing) {
+            PaletteManager()
+        }
     }
+    
+ //   @State private var editing = false
+    @State private var managing = false
+    @State private var paletteToEdit: Palette?
     
     var rollTransition: AnyTransition {
         AnyTransition.asymmetric(
